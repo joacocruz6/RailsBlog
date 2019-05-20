b89d424bff88b3b29675cf29d556130e6a4e750c
Joaquin Cruz <joacocruz6@gmail.com>
2019-05-19 22:46:25 -0400

    Advanced on rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'posts#index'
+  root 'posts#index', as: 'home'
   resources :posts
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: 'about'
 end
